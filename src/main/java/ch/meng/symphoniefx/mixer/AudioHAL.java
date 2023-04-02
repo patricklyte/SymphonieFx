58c7f10ca3e28217d565ca6744826a38f92383e0
patricklyte <patrickmeng@bluewin.ch>
2023-04-02 15:42:21 +0200

    Show more audio devices

--- src/main/java/ch/meng/symphoniefx/mixer/AudioHAL.java
+++ src/main/java/ch/meng/symphoniefx/mixer/AudioHAL.java
@@ -1,19 +1,11 @@
 package ch.meng.symphoniefx.mixer;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Line;
 import javax.sound.sampled.Mixer;
-import javax.sound.sampled.TargetDataLine;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
 
 public class AudioHAL {
-    private static final Logger logger = LogManager.getLogger();
-
     private List<String> asioDrivers = new Vector<>();
     private List<String> javaDrivers = new Vector<>();
     private List<Mixer.Info> javaDevices = new Vector<>();
@@ -21,32 +13,28 @@ public class AudioHAL {
     public List<String> getAudioOutputDevices() {
         javaDevices.clear();
         javaDrivers.clear();
-        Line.Info info = new Line.Info(TargetDataLine.class);
-        for(Line.Info lineInfo : AudioSystem.getTargetLineInfo(info)) {
-            logger.debug(lineInfo);
+        for (Mixer.Info mixerinfo : AudioSystem.getMixerInfo()) {
+            if (!isSpeaker(mixerinfo) || isCapture(mixerinfo)) continue;
+            javaDevices.add(mixerinfo);
+            javaDrivers.add(mixerinfo.getName() + ":" + mixerinfo.getDescription());
         }
-        Mixer defaultMixer = AudioSystem.getMixer(null);
-        addAudioDevice(defaultMixer.getMixerInfo(), " (DEFAULT)");
-        Arrays.stream(AudioSystem.getMixerInfo()).filter(this::isOutputAudioDevice).forEach(this::addAudioDevice);
+        for (Mixer.Info mixerinfo : AudioSystem.getMixerInfo()) {
+            if (isSpeaker(mixerinfo) || isCapture(mixerinfo)) continue;
+            javaDevices.add(mixerinfo);
+            javaDrivers.add(mixerinfo.getName() + ":" + mixerinfo.getDescription());
+        }
         List<String> temp = new Vector<>();
         temp.addAll(javaDrivers);
         return temp;
     }
 
-    private void addAudioDevice(Mixer.Info mixerInfo, final String postfix) {
-        javaDevices.add(mixerInfo);
-        javaDrivers.add(mixerInfo + ":" + mixerInfo.getDescription() + postfix);
+    private boolean isSpeaker(Mixer.Info mixerinfo) {
+        return mixerinfo.getDescription().toLowerCase().contains("playback")
+                || mixerinfo.getDescription().toLowerCase().contains("speaker");
     }
 
-    private void addAudioDevice(Mixer.Info mixerInfo) {
-        addAudioDevice(mixerInfo, "");
-    }
-
-    private boolean isOutputAudioDevice(Mixer.Info mixerInfo) {
-        if (mixerInfo.getDescription().toLowerCase().contains("playback")
-                || mixerInfo.getDescription().toLowerCase().contains("speaker")) return true;
-        Mixer mixer = AudioSystem.getMixer(mixerInfo);
-        return mixer.getSourceLineInfo().length == 0;
+    private boolean isCapture(Mixer.Info mixerinfo) {
+        return mixerinfo.getDescription().toLowerCase().contains("capture");
     }
 
     public Mixer.Info getMixerInfo(int index) {
